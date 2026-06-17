eed21bc3b709c939878dd12f4770bfabc488ab64
penguinjean0421 <penguinjean0421@gmail.com>
2026-06-18 02:58:22 +0900

    파티클 추가

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,7 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+
+    [Header("VFX References")]
+    public GameObject clearParticlePrefab;
+    public float particleScaleMultiplier = 1f;
+
+    [Header("Clear Score")]
     public int stageClearScore;
 
     PlayerHealth playerHealth;
@@ -25,14 +32,10 @@ public class PlayerInteraction : MonoBehaviour
     {
         if (collision.CompareTag("Item")) { HandleItem(collision.gameObject); }
 
-        else if (collision.CompareTag("Finish"))
+        if (collision.CompareTag("Finish"))
         {
-            // 파티클 실행
-
-            GameManager.instance.score += stageClearScore;
-            UIManager.instance.UpdateScore(GameManager.instance.score);
-
-            GameManager.instance.NextStage();
+            collision.enabled = false;
+            StartCoroutine(FinishSequenceRoutine(collision.gameObject));
         }
 
         if (collision.CompareTag("DeadZone"))
@@ -40,6 +43,29 @@ public class PlayerInteraction : MonoBehaviour
             GameManager.instance.HealthDown();
             GameManager.instance.PlayerReposition();
         }
+    }
+
+    IEnumerator FinishSequenceRoutine(GameObject finishObj)
+    {
+        if (clearParticlePrefab != null)
+        {
+            int spawnCount = 4;
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Vector3 randomOffset = new Vector3(Random.Range(-0.6f, 0.6f), Random.Range(-0.2f, 0.6f), 0f);
+                GameObject particleGo = Instantiate(clearParticlePrefab, finishObj.transform.position + randomOffset, Quaternion.identity);
+                particleGo.transform.localScale *= particleScaleMultiplier;
+                DontDestroyOnLoad(particleGo);
+                Destroy(particleGo, 3.0f);
+            }
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        GameManager.instance.score += stageClearScore;
+        UIManager.instance.UpdateScore(GameManager.instance.score);
+
+        GameManager.instance.NextStage();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
